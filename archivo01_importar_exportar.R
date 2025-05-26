b93480f0f3ea4ebe7875d6f31ab79515c4ffcfcd
smata2201 <santim2006@gmail.com>
2025-05-25 23:15:37 -0600

    IGNORAR

--- archivo01_importar_exportar.R
+++ archivo01_importar_exportar.R
@@ -1,18 +1,1 @@
-# Importación y exploración inicial de datos
 
-# Cargar datos desde el archivo CSV
-datos <- read.csv("Desktop/dataset.csv", stringsAsFactors = FALSE)
-
-# Mostrar resumen de los datos
-summary(datos)
-
-# Mostrar dimensiones del data frame
-dim(datos)
-
-# Extraer columnas numéricas
-columnas_numericas <- datos[sapply(datos, is.numeric)]
-print(columnas_numericas)
-
-# Calcular media de cada columna numérica
-medias <- colMeans(columnas_numericas, na.rm = TRUE)
-print(medias)
